92318830a8a14ee20fe2236f46173e5e12eaa54f
Gregory Robertson <robertsongregorya@gmail.com>
2021-05-24 09:19:14 -0400

    Implement Graphics draw

--- src/src/graphics/graphics.cpp
+++ src/src/graphics/graphics.cpp
@@ -5,5 +5,9 @@ Graphics::Graphics() {
 }
 
 void Graphics::Draw(SDL_Renderer* renderer) {
-
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    SDL_RenderClear(renderer);
+    for (size_t i = 0; i < this->actors.size(); ++i) {
+        this->actors[i]->Draw(renderer);
+    }
 }
